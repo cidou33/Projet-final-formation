e35e113d9a39e0e7adb53eb56e9d6326ab446016
cedric ducros <cedricducros1@gmail.com>
2022-04-25 11:12:44 +0200

    ajout de l'update user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,12 +7,12 @@ use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UserController extends AbstractController
 {
     #[Route('/acceuil', name: 'homePage', methods: ['GET', 'POST'])]
     public function redirectHomePage(){
-
         $user = $this->getUser();
         $connect = $this->getUser() == null;
         return $this->render('accueil.html.twig', [
@@ -37,7 +37,56 @@ class UserController extends AbstractController
                 'id' => $this->getUser()->getId()
             ]);
         }
+    }
 
+    #[Route('/mon-profil', name: 'updateUser', methods: ['GET', 'POST'])]
+    public function modifyUser(UsersRepository $usersRepository, Request $request, SluggerInterface $slugger){
+        $user = $this->getUser();
+        if($user != null) {
+            $userId = $user->getId();
+            $connect = $this->getUser() == null;
+            $userUpdate = $usersRepository->findOneBy(['id' => $userId]);
+            $oldImg = $userUpdate->getImgProfil();
+            if (!empty($request->request->all())) {
+                $username = $request->request->get('username');
+                $userUpdate->setUsername($username);
+                $img = $request->files->get('avatar');
+                if ($img == null) {
+                    $img = $oldImg;
+                    $userUpdate->setImgProfil($img);
+                    $usersRepository->add($userUpdate);
+                    return $this->redirectToRoute('homePage', [
+                        'user' => $user,
+                        'connect' => $connect,
+                    ]);
+                }
+                $originalFilename = pathinfo($img[0]->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . md5(uniqid()) . '.' . $img[0]->guessExtension();
+                $img[0]->move($this->getParameter('uploadDirectory'), $newFilename);
+                $userUpdate->setImgProfil($newFilename);
+                $usersRepository->add($userUpdate);
+                return $this->redirectToRoute('homePage', [
+                    'user' => $user,
+                    'connect' => $connect,
+
+
+                ]);
+            }
+            return $this->render('user/profil.html.twig', [
+                'user' => $user,
+                'connect' => $connect
+            ]);
+        }
+        return $this->redirectToRoute('homePage');
+    }
+
+    #[Route('effacer-son-compte', name: 'deleteUser', methods: ['GET', 'POST'])]
+    public function deleteUser(UsersRepository $usersRepository,){
+        $userId = $this->getUser()->getId();
+        $userDeleted = $usersRepository->findOneBy(['id'=> $userId]);
+        $usersRepository->remove($userDeleted);
+        return $this->redirectToRoute('homePage');
     }
 
     #[Route('/toto/{id}', name: 'toto', methods: ['GET', 'POST'])]
@@ -55,4 +104,6 @@ class UserController extends AbstractController
             'transcriptions' => $transcriptions
         ]);
     }
+
+
 }
